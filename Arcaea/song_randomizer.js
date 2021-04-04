3f00dd928242a6564b6b821958401a7c05400882
Buck4437 <Buck4437@users.noreply.github.com>
2021-04-04 14:28:30 +0800

    code

--- Arcaea/song_randomizer.js
+++ Arcaea/song_randomizer.js
@@ -1,11 +1,11 @@
-songs = []
-DIFFICULTIES = ["past", "present", "future", "beyond"]
+var songs = []
+const DIFFICULTIES = ["past", "present", "future", "beyond"]
 
 fetch(`https://arcaea.fandom.com/api.php?action=parse&pageid=1014&prop=sections&format=json`)
 	.then(a => a.json())
 	.then(a => this.getSongs(a))
 
-getSongs = (data) => {
+var getSongs = (data) => {
   if (data.error !== undefined) return
   for (let s of data.parse.sections) {
     	if (DIFFICULTIES.includes(s.line.toLowerCase())) {
@@ -16,14 +16,14 @@ getSongs = (data) => {
   }
 }
 
-parse = (data) => {
+var parse = (data) => {
 	if (data.error !== undefined) return
 	songs = songs.concat(data.parse.wikitext["*"].replaceAll(/[\{\}\[\]]/g, "").replaceAll("'''", "").split("|-\n").filter((a, i) => i !== 0).map(f => f.replaceAll("| ", "").replaceAll(/data-sort-value=\"\d+.\d+\"/g, "").split("\n").map(t => t.trim()).filter((s, c) => c <= 3)))
 }
 
-randomizer = (data, min = 0, max = 100) => {
+var randomizer = (data, min = 0, max = 100) => {
   let arr = data.filter(s => !isNaN(Number(s[2]))).map(s => [s[0].match(/\|/g) ? s[0].split("|")[1] : s[0], s[1], Number(s[2]), `Level ${s[3].replaceAll("|", "")}`]).filter(s => s[2] >= min && s[2] <= max)
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-run = (min, max) => randomizer(songs, min, max)
+var run = (min, max) => randomizer(songs, min, max)
