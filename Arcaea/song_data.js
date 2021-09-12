d241f9b08688c00e72b0179dc375d99f7ea4eac3
Buck4437 <Buck4437@users.noreply.github.com>
2021-09-13 02:04:21 +0800

    backup

--- Arcaea/song_data.js
+++ Arcaea/song_data.js
@@ -5,24 +5,81 @@
 
 
 
+
+
+
+
+
+
+table = {}
+function addToTable(name, difficulty, count) {
+    if (table[name] === undefined) {
+        table[name] = {};
+    }
+    table[name][difficulty] = count;
+}
+
+function query(name, difficulty) {
+    console.log(`Querying ${name} ${difficulty}`)
+    return table[name][difficulty];
+}
+
+function addNoteCount(link, name, difficulty) {
+    console.log("Adding " + name)
+    let pageName = link.split("/").reverse()[0]
+
+    // TODO: FIX THIS PART
+
+    fetch(`https://arcaea.fandom.com/api.php?action=query&titles=${name}&format=json`)
+    .then(a => {
+        pages = a.json().query.pages
+        return pages.keys[0]
+    })
+    .then(id => {
+        let args = [
+            "action=parse",
+            `page=${pageName}`,
+            "prop=wikitext",
+            "format=json"
+        ]
+        return fetch(`https://arcaea.fandom.com/api.php?${args.join("&")}`)
+    })
+    .then(a => a.json())
+    .then(a => {
+        const regex = new RegExp(`${difficulty} Combo = \\d+`, "g");
+        let substr = a.parse.wikitext["*"].match(regex)[0]
+        let count = substr.split(" = ")[1]
+        this.addToTable(name, difficulty, count)
+    })
+}
+
 class Song {
-    constructor(id, name, ftr, byd, note_ftr, note_byd) {
+    constructor(id, name, ftr, byd, link) {
         this.id = id
         this.name = name
         this.ftr = ftr
-        this.note_ftr = note_ftr
         this.byd = byd
-        this.note_byd = note_byd
+        this.link = link
+        addNoteCount(link, name, "Future")
+        if (this.byd !== "") {
+            addNoteCount(link, name, "Beyond")
+        }
     }
 
     toString() {
         let diffs = []
         if (this.byd === "") {
-            diffs.push(this.ftr)
+            diffs.push(["Future", this.ftr])
         } else {
-            diffs = diffs.concat(this.ftr, this.byd)
+            diffs = diffs.concat(["Future", this.ftr], ["Beyond", this.byd])
         }
-        return diffs.map(diff => [this.id, this.name, diff].join("\t")).join("\n");
+        return diffs.map(diff => [
+                this.id,
+                this.name,
+                diff[1],
+                query(this.name, diff[0])
+            ].join("\t")
+        ).join("\n");
     }
 }
 
@@ -46,8 +103,10 @@ function copyText(text) {
     document.body.removeChild(el);
 }
 
+
+
 getText = (nodes, id) => nodes.item(id).textContent.replaceAll("\n", "")
-getLink = nodes => nodes.item(2)
+getLink = nodes => nodes.item(2).querySelector("a").href
 
 songs = []
 el = document.querySelector(".songbydate-table")
@@ -58,10 +117,11 @@ for (let row of rows) {
     let name = getText(cells, 2)
     let ftr = getText(cells, 6)
     let byd = getText(cells, 7)
+    let link = getLink(cells)
     songs.push(new Song(
-        id, name, ftr, byd
+        id, name, ftr, byd, link
     ))
 }
 
-wtf = songs.map(song => song.toString()).join("\n")
-copyText(wtf)
+// wtf = songs.map(song => song.toString()).join("\n")
+// copyText(wtf)
