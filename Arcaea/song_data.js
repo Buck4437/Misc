aae236f64655ab704fb9302d451ae22da5c8b2da
Buck4437 <Buck4437@users.noreply.github.com>
2022-09-16 17:51:00 +0800

    Update song_data.js

--- Arcaea/song_data.js
+++ Arcaea/song_data.js
@@ -1,4 +1,7 @@
 table = {}
+requestReceived = 0
+requestProcessed = 0
+
 function addToTable(name, difficulty, count) {
     if (table[name] === undefined) {
         table[name] = {};
@@ -7,18 +10,22 @@ function addToTable(name, difficulty, count) {
 }
 
 function query(name, difficulty) {
-    console.log(`Querying ${name} ${difficulty}`)
+    // console.log(`Querying ${name} ${difficulty}`)
+    if (name == "Last" && difficulty == "Beyond") {
+        return [query("Last | Moment", "Beyond"), query("Last | Eternity", "Beyond")].join("/")
+    }
     if (table[name] === undefined || table[name][difficulty] === undefined) {
         return "Unknown"
     }
     return table[name][difficulty];
 }
 
-function addNoteCount(link, name, difficulty) {
-    console.log("Adding " + name)
-    let pageName = link.split("/").reverse()[0]
-
-    // TODO: FIX THIS PART
+function addNoteCount(link, name, difficulty, redirect=false) {
+    if (!redirect) {
+        requestReceived += 1;
+    }
+    console.log("Adding " + name + (redirect ? " (Redirected)" : ""))
+    let pageName = (redirect ? link : link.split("/").reverse()[0])
 
     fetch(`https://arcaea.fandom.com/api.php?action=query&titles=${pageName}&format=json`)
     .then(a => a.json())
@@ -42,23 +49,73 @@ function addNoteCount(link, name, difficulty) {
     })
     .then(a => a.json())
     .then(a => {
-        const regex = new RegExp(`${difficulty}Combo=\\d+`, "g");
-        let rawText = a.parse.wikitext["*"].replaceAll(/\s/g, "")
-        let matches = rawText.match(regex)
-        if (matches === null) {
-            console.log("Regex not found for this song:")
-            console.log(rawText)
-            console.log(regex)
-        } else {
-            let count = matches[0].split("=")[1]
-            this.addToTable(name, difficulty, count)
+        let text = a.parse.wikitext["*"]
+        let requests = parseWikiText(name, text, difficulty)
+        for (let request of requests) {
+            if (request.header === "Success") {
+                this.addToTable(request.name, request.difficulty, request.count)
+                console.log("Added song " + request.name)
+            } else if (request.header === "Redirect") {
+                addNoteCount(request.name, name, difficulty, true)
+                return
+            } else {
+                console.log("Failed for song " + request.name)
+            }
         }
+        requestProcessed += 1;
     })
 }
 
+class Request {
+
+    constructor(header, name, difficulty, count) {
+        this.header = header;
+        this.name = name;
+        this.difficulty = difficulty;
+        this.count = count;
+    }
+
+}
+
+// this function takes raw data and returns corresponding combo, a redirect notice, or null
+function parseWikiText(name, text, difficulty) {
+    let rawText = text.replaceAll(/\s/g, "")
+
+    let potentialRegex = new RegExp(`${difficulty}Combo=(Touch:)?\\d+`, "g")
+
+    let redirectRegex = new RegExp(`#REDIRECT.{0,2}\\[\\[[^\\]]*\\]\\]`, "g")
+
+    // If difficulty is beyond, use these
+    let lastRegex = [
+        new RegExp(`MomentCombo=\\d+`, "g"),
+        new RegExp(`EternityCombo=\\d+`, "g")
+    ]
+
+    let potentialMatch = rawText.match(potentialRegex);
+    let redirectMatch = rawText.match(redirectRegex);
+    let lastMatches = lastRegex.map(x => rawText.match(x));
+
+    if (potentialMatch !== null) {
+        let count = potentialMatch[0].split("=")[1].replaceAll("Touch:", "");
+        return [new Request("Success", name, difficulty, count)];
+    } else if (redirectMatch !== null) {
+        let betterRegex = new RegExp("\\[\\[[^\\]]*\\]\\]", "g")
+        let match = text.match(betterRegex);
+        let name = match[0].replaceAll("]]", "").replaceAll("[[", "");
+
+        return [new Request("Redirect", name, difficulty, "0")];
+    } else if (lastMatches[0] !== null && lastMatches[1] !== null) {
+        let counts = lastMatches.map(x => x[0].split("=")[1])
+        return [
+            new Request("Success", "Last | Moment", difficulty, counts[0]),
+            new Request("Success", "Last | Eternity", difficulty, counts[1])
+        ]
+    }
+    return [new Request("Failure", name, difficulty, "0")]
+}
+
 class Song {
-    constructor(id, name, ftr, byd, link) {
-        this.id = id
+    constructor(name, ftr, byd, link) {
         this.name = name
         this.ftr = ftr
         this.byd = byd
@@ -77,7 +134,6 @@ class Song {
             diffs = diffs.concat([["Future", this.ftr], ["Beyond", this.byd]])
         }
         return diffs.map(diff => [
-                this.id,
                 this.name,
                 diff[1],
                 query(this.name, diff[0])
@@ -109,7 +165,7 @@ function copyText(text) {
 songs = []
 
 function run() {
-    getText = (nodes, id) => nodes.item(id).textContent.replaceAll("\n", "")
+    getText = (nodes, id) => nodes.item(id).textContent
     getLink = nodes => nodes.item(1).querySelector("a").href
 
     songs = []
@@ -117,18 +173,30 @@ function run() {
     rows = el.querySelectorAll("tbody tr")
     for (let row of rows) {
         let cells = row.querySelectorAll("td")
-        let id = getText(cells, 0)
         let name = getText(cells, 1)
         let ftr = getText(cells, 5)
         let byd = getText(cells, 6)
-        console.log(byd)
         let link = getLink(cells)
         songs.push(new Song(
-            id, name, ftr, byd, link
+            name, ftr, byd, link
         ))
     }
+
+    interval = setInterval(() => {
+        console.log(`Request Received: ${requestReceived}
+Request Processed: ${requestProcessed}
+Progress: ${requestProcessed / requestReceived * 100}%`)
+        if (requestReceived > 0 && requestReceived === requestProcessed) {
+            clearInterval(interval)
+            console.log("Request completed.")
+        }
+    }, 5000)
 }
 
 run()
-// wtf = songs.map(song => song.toString()).join("\n")
-// copyText(wtf)
+
+// Run this when all song data has been fetched
+function copy() {
+    songAsExcel = songs.map(song => song.toString()).join("\n")
+    copyText(songAsExcel)
+}
